7a372fcaa992901c450906e33a4895d75f6f879f
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-23 19:48:10 -0300

    pyudemy

--- pep8.py
+++ pep8.py
@@ -1,2 +1,5 @@
-# Index of Python Enhancement Proposals
-# Índice de propostas de aprimoramento do Python
+# Index of Python Enhancement Proposals / Índice de propostas de aprimoramento do Python
+# São propostas de melhorias para a linguagem Python
+
+# Poema The Zen of Python, comando = import this
+# import this
