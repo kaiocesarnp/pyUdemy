9a93f6d0766c4cd41e7000944db92e6b31fc5320
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-23 19:52:04 -0300

    pyudemy

--- pep8.py
+++ pep8.py
@@ -3,3 +3,7 @@
 
 # Poema The Zen of Python, comando = import this
 # import this
+
+# A ideia da PEP8 é que possamos escrever códigos Python de forma pythonica, ou seja, bonita. Segundo o The Zen of Python
+
+# 1 - Utilize Camel Case para nomes de classe;
