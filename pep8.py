b46e0300d033ac31fb33a7e899c2eae96a473dae
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-23 20:07:17 -0300

    pyudemy

--- pep8.py
+++ pep8.py
@@ -6,4 +6,18 @@
 
 # A ideia da PEP8 é que possamos escrever códigos Python de forma pythonica, ou seja, bonita. Segundo o The Zen of Python
 
-# 1 - Utilize Camel Case para nomes de classe;
+# 1 - Utilize Camel Case para nomes de classes;
+    # Ex: class Calculadora:
+    #     class CalculadoraCientifica:
+
+# 2 - Utilize nomes em minúsculo, separados por underline para funções ou variáveis;
+# def soma():
+# def soma_dois():
+# numero = 4
+# numero_impar = 5
+
+# 3 - Utilize 4(quatro) espaços para identação
+
+# if 'a' in 'banana':
+    # print('tem')
+# ^^^4 espaços    
