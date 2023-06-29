fb547ee2f066d2f8535810245b421def4692a671
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-29 12:36:26 -0300

    pyudemy

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -21,3 +21,8 @@ idade = input()
 # print('O(A) {0} tem {1} anos.'.format(nome, idade))# Exemplo de print 'moderno' 3.x
 
 print(f'O(A) {nome} tem {idade} anos.') # Exemplo de print 'mais atual' 3.7x
+
+# int(idade) => cast
+# Cast é a 'conversão' de um tipo de dado para outro
+print(f'O(A) {nome} nasceu em {2023 - int(idade)}')
+
