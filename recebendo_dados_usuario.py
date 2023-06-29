71728ce66994d278eb063e83b1ac2375e24340d5
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-29 12:31:31 -0300

    pyudemy

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -8,7 +8,9 @@ nome = input().title()
 
 # print('Seja bem-vindo(a) %s' % nome) # Exemplo de print 'antigo' 2.x
 
-print('Seja bem-vindo(a) {0}'.format(nome)) # Exemplo de print 'moderno' 3.x
+# print('Seja bem-vindo(a) {0}'.format(nome)) # Exemplo de print 'moderno' 3.x
+
+print(f'Seja bem-vindo(a) {nome}') # Exemplo de print 'mais atual' 3.7x
 
 print("Qual sua idade? ")
 idade = input()
@@ -16,4 +18,6 @@ idade = input()
 # Saída de dados
 # print("O(A) %s tem %s anos." % (nome, idade)) # Exemplo de print 'antigo' 2.x
 
-print('A {0} tem {1} anos.'.format(nome, idade))# Exemplo de print 'moderno' 3.x
+# print('O(A) {0} tem {1} anos.'.format(nome, idade))# Exemplo de print 'moderno' 3.x
+
+print(f'O(A) {nome} tem {idade} anos.') # Exemplo de print 'mais atual' 3.7x
