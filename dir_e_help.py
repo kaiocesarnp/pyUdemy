e16ef4edd35477182b57d693a296538086bac0a3
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-29 12:09:27 -0300

    Pyudemy

--- dir_e_help.py
+++ dir_e_help.py
@@ -1,11 +1,21 @@
 """
 Utilitários Python para auxiliar na programação:
 
-Dir > Apresenta todos os atributos/propriedades e funções/métodos disponíveis para 
+dir > Apresenta todos os atributos/propriedades e funções/métodos disponíveis para 
         determinado tipo de dado ou variável;
 
-Help > Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos 
+dir(tipo de dado/variavel) = dir("Geek")
+
+help > Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos 
         disponíveis para determinado tipo de dado ou variável.
 
+help("Geek".lower) = lower é só um exemplo do que pode ser feito. Infos obtidas através do dir
+    lower no caso é um método de letras minúsculas.
+
+"GeEk".lower() = 'geek'
+
 """
+
+print(dir('geek'))
+
 
