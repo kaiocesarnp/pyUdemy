adb4adffc831020a44aeae151d6667b04d885f70
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-29 12:41:30 -0300

    Pyudemy

--- recebendo_dados_usuario.py
+++ recebendo_dados_usuario.py
@@ -3,8 +3,10 @@ Recebendo dados do usuário
 """
 
 # Entrada de dados
-print("Qual seu nome? ")
-nome = input().title()
+# print("Qual seu nome? ")
+# nome = input().title()
+
+nome = input('Qual seu nome? ').title()
 
 # print('Seja bem-vindo(a) %s' % nome) # Exemplo de print 'antigo' 2.x
 
@@ -12,17 +14,22 @@ nome = input().title()
 
 print(f'Seja bem-vindo(a) {nome}') # Exemplo de print 'mais atual' 3.7x
 
-print("Qual sua idade? ")
-idade = input()
+# print("Qual sua idade? ")
+# idade = input()
+
+idade = input('Qual sua idade? ')
 
 # Saída de dados
 # print("O(A) %s tem %s anos." % (nome, idade)) # Exemplo de print 'antigo' 2.x
 
 # print('O(A) {0} tem {1} anos.'.format(nome, idade))# Exemplo de print 'moderno' 3.x
 
-print(f'O(A) {nome} tem {idade} anos.') # Exemplo de print 'mais atual' 3.7x
+# print(f'O(A) {nome} tem {idade} anos.') # Exemplo de print 'mais atual' 3.7x
 
 # int(idade) => cast
 # Cast é a 'conversão' de um tipo de dado para outro
-print(f'O(A) {nome} nasceu em {2023 - int(idade)}')
+# print(f'O(A) {nome} nasceu em {2023 - int(idade)}')
+
+
+print(f'O(A) {nome} tem {idade} anos e, nasceu em {2023 - int(idade)}.')
 
