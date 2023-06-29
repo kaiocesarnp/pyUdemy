306da5e4ff61eb5af97f0f9d3cb192ccc2542eb3
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-29 11:55:10 -0300

    pyudemy

---
+++ dir_e_help.py
@@ -0,0 +1,11 @@
+"""
+Utilitários Python para auxiliar na programação:
+
+Dir > Apresenta todos os atributos/propriedades e funções/métodos disponíveis para 
+        determinado tipo de dado ou variável;
+
+Help > Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos 
+        disponíveis para determinado tipo de dado ou variável.
+
+"""
+
