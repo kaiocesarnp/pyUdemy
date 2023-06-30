ee833d9a45d46de09e46164413cd8d02c888f324
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-30 12:28:10 -0300

    pyudemy

---
+++ s4_variaveis_e_dados/tipo_numerico.py
@@ -0,0 +1,24 @@
+"""
+TIPO NUMÉRICO
+"""
+
+num = 1_000_000
+print(num)
+
+# Errado, tuple
+valor = 1, 44
+print(valor)
+print(type(valor))
+
+# Certo, float
+valor = 1.44
+print(valor)
+print(type(valor))
+
+# Dupla atribuição
+valor1, valor2 = 1, 44
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
+
