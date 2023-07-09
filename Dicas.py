88fe0c2517a9aca4b17b27823ae0b7c2f579cc5a
kaiocesarnp <kaiocesarnp@gmail.com>
2023-07-09 16:33:42 -0300

    pyudemy

--- Dicas.py
+++ Dicas.py
@@ -1,5 +1,8 @@
 # DICAS
-
+nome = 'Geek University'
 # print(nome.split()) = Transforma em uma lista de strings
 
 # print(nome[5:15]) = chamado de Slice de string
+
+# Começando do primeiro elemento, indo até o último e invertendo:
+print(nome[::-1])
