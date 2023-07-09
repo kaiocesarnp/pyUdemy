397c900fdad2ae7d57123e2356c0e66297b7888c
kaiocesarnp <kaiocesarnp@gmail.com>
2023-07-09 16:54:45 -0300

    pyudemy

---
+++ s4_variaveis_e_dados/escopo_de_variaveis.py
@@ -0,0 +1,24 @@
+"""
+Dois casos de escopo:
+
+1 - Variáveis globais:
+    - São reconhecidas, ou seja, seu escopo compreende todo o programa.
+
+2 - Variáveis locais:
+    São reconhecidas apenas no bloco onde foram declaradas, ou seja, seu escopo
+        está limitado ao bloco onde foi declarada
+    
+Para declararmos variáveis em Python fazemos:
+nome_da_variavel = valor_da_variavel
+
+Python é uma linguagem de tipagem dinamica. Isso significa que ao declararmos uma
+variável, nós não colocamos o tupo de dado dela.
+Este tipo é inferido ao atribuirnos o valor à mesma.
+
+Exemplo em C:
+int numero = 42;
+
+Exemplo em Java:
+int numero = 42;
+"""
+
