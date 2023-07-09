8f538cf63e43472a0eaa18477ac217d602600cd1
kaiocesarnp <kaiocesarnp@gmail.com>
2023-07-09 16:43:21 -0300

    pyudemy

--- Dicas.py
+++ Dicas.py
@@ -1,12 +1,23 @@
 # DICAS
+
+"""
 nome = 'Geek University'
-# print(nome.split()) = Transforma em uma lista de strings
+print(nome.split()) = Transforma em uma lista de strings
 
-# print(nome[5:15]) = chamado de Slice de string
+print(nome[5:15]) = chamado de Slice de string
 
-# Começando do primeiro elemento, indo até o último e invertendo:
-# print(nome[::-1])
+Começando do primeiro elemento, indo até o último e invertendo:
+print(nome[::-1])
 
-# Substituir uma letra por outra. Se tiver mais letras que serão substituídas e são iguais
-    # a nova letra substituirá todas
+Substituir uma letra por outra. Se tiver mais letras que serão substituídas e são iguais
+    a nova letra substituirá todas
 print(nome.replace('G', 'P'))
+
+"""
+
+# -----------------------------------------
+
+# Palindromo
+texto = 'socorram me subino onibus em marrocos'
+print(texto)
+print(texto[::-1])
