d2d2b71314b12ddc5cf708b9c53bc717e7f64f78
kaiocesarnp <kaiocesarnp@gmail.com>
2023-07-09 16:38:55 -0300

    pyUdemy

--- Dicas.py
+++ Dicas.py
@@ -5,4 +5,8 @@ nome = 'Geek University'
 # print(nome[5:15]) = chamado de Slice de string
 
 # Começando do primeiro elemento, indo até o último e invertendo:
-print(nome[::-1])
+# print(nome[::-1])
+
+# Substituir uma letra por outra. Se tiver mais letras que serão substituídas e são iguais
+    # a nova letra substituirá todas
+print(nome.replace('G', 'P'))
