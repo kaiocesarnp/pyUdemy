aa75b99d3082b69f73a1a1b486cc237d63d4d1ae
Kaio <kaiocesarnp@gmail.com>
2023-07-18 12:31:22 -0300

    pyudemy

---
+++ s5_logicas_e_condicionais/loop_for.py
@@ -0,0 +1,63 @@
+"""
+Loop e for
+
+Loop > Estrutura de repetição
+For > Uma dessas estruturas
+
+Utiliza-se Loops paraa iterar sobre sequências ou sobre valores iteráveis
+Exemplos de iteráveis:
+-String
+   nome = 'Geek University'
+-Lista
+   lista = [1, 3, 5, 7, 9]
+- Range
+   numeros = range(1, 10)
+
+"""
+nome = 'Geek University'
+lista = [1, 3, 5, 7, 9]
+numeros = range(1, 10) #Temos que transformar numa lista
+
+#Exemplo de for 1 (iterando em uma string)
+for letra in nome:
+    print(letra)
+
+#Exemplo de for 2 (iterando sobre uma lista)
+for numero in lista:
+    print(numero)
+
+#Exemplo de for 3 (iterando sobre um range)
+#Obs: O valor final não é incluío
+for numero in range(1, 10):
+    print(numero)
+
+
+#Qual é o indice?
+#O 'enumerate' gera para cada sequência do iterável, um índice
+#((0, 'G'), (1, 'e'), (2, 'e'), (3, 'k'), (4, ' '), (5, 'U')...)
+for indice, letra in enumerate(nome):
+    print(nome[indice])
+
+#Outro método:
+for indice, letra in enumerate(nome):
+    print(letra)
+
+#Outro método:
+#Obs: Quando há um valor que pode ser descartado, utiliza-se o underline.
+for _, letra in enumerate(nome):
+    print(letra)
+
+#Printando indice e valor
+for valor in enumerate(nome):
+    print(valor[0]) #somente o indice, numeros
+    print(valor[1]) #somente a letra
+
+
+
+
+
+
+
+
+
+
