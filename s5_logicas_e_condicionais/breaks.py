ed5c35e67cc1cf249bd96e2720c2006c6fae575f
Kaio <kaiocesarnp@gmail.com>
2023-07-18 16:31:06 -0300

    pyudemy

---
+++ s5_logicas_e_condicionais/breaks.py
@@ -0,0 +1,56 @@
+"""
+Saindo de loops com break:
+Utiliza-se o break para sair de loops de maneira projetada.
+
+#exemplo 1:
+for numero in range(1, 11):
+    if numero == 6:
+        break
+    else:
+        print(numero)
+print('Sai do loop')
+
+
+"""
+
+#exemplo 2:
+while True:
+    comando = input("Digite 'sair' para sair: ")
+    if comando == 'sair':
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
