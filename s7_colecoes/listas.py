ac2956eed88a79733c1bd783b88996dd1d26cada
Kaio <kaiocesarnp@gmail.com>
2023-07-19 16:57:28 -0300

    pyudemy

--- s7_colecoes/listas.py
+++ s7_colecoes/listas.py
@@ -15,6 +15,7 @@ lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 
 lista3 = []
 lista4 = list(range(11))
 lista5 = list('Geek University')
+lista6 = [1, 2.34, True, 'Geek', 'd', [1, 2, 3], 45345345345] #lista com diversos tipos de dados
 
 
 """
@@ -158,7 +159,63 @@ print(curso)
 
 #--------------
 
+#Convertendo uma lista em string
+lista6 = ['Programação', 'em', 'Python:', 'Essencial']
+print(lista6)
+
+#curso = ' '.join(lista6) #Adiciona espaço entre os elementos e tranforma em string
+curso = '$'.join(lista6) #Adiciona cifrão entre os elementos e tranforma em string
+print(curso)
+
+#--------------
+
+#Iterando sobre listas
+
+#Utilizando 'for'
+soma = 0
+for elemento in lista4:
+    print(elemento)
+    soma = soma + elemento
+print(soma)
+
+soma = ''
+for elemento in lista2:
+    print(elemento)
+    soma = soma + elemento
+print(soma)
+
+#Utilizando while
+carrinho = [] #cria uma lista vazia
+produto = ''  #cria uma variavel do tipo string
+
+while produto != 'sair':
+    print("Adicione um produto na lista ou digite 'sair' para sair: ")
+    produto = input()
+    if produto != 'sair':
+        carrinho.append(produto)
+
+	#quando o usuário digitar sair, sai do loop while e vai para o loop for
+for produto in carrinho:
+    print(produto)
+
+#--------------
+
 """
+
+#Utilizando variáveis em listas
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+numeros = [num1, num2, num3, num4, num5]
+print(numeros)
+
+
+
 
 
 
